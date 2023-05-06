8113f85ac93c049744f571d2ecb8fb029eefe0ff
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-05-06 01:07:08 -0600

    Update Toast message options

--- src/providers/DnDProvider.jsx
+++ src/providers/DnDProvider.jsx
@@ -4,22 +4,36 @@ import { useToastContext } from "./ToastProvider";
 
 const DnDContext = createContext({});
 
+const endpointFetchOptions = (title) => {
+  return {
+    pending: `Loading D&D ${title}...`,
+    success: `D&D ${title} loaded!`,
+    error: `D&D ${title} loading error!`,
+  };
+};
+
 export const DnDProvider = ({ children }) => {
   const [loadingData, setLoadingData] = useState({});
   const [data, setData] = useState({});
   const { toastPromise } = useToastContext();
 
   const fetchDnDEndpoints = () =>
-    toastPromise(fetchDnD("/api"))
-      .then((response) => response.json())
-      .then((resolve) => setLoadingData(resolve));
+    toastPromise(
+      fetchDnD("/api")
+        .then((response) => response.json())
+        .then((resolve) => setLoadingData(resolve)),
+      endpointFetchOptions("endpoint data")
+    );
 
   const fetchEndpointData = (endpoint, label) =>
-    toastPromise(fetchDnD(endpoint))
-      .then((response) => response.json())
-      .then((resolve) =>
-        setData((prevData) => ({ ...prevData, [`${label}Data`]: resolve }))
-      );
+    toastPromise(
+      fetchDnD(endpoint)
+        .then((response) => response.json())
+        .then((resolve) =>
+          setData((prevData) => ({ ...prevData, [`${label}Data`]: resolve }))
+        ),
+      endpointFetchOptions(label.charAt(0).toUpperCase() + label.slice(1))
+    );
 
   const fetchDnDSpells = () => fetchEndpointData(loadingData.spells, "spells");
 
@@ -31,6 +45,7 @@ export const DnDProvider = ({ children }) => {
     <DnDContext.Provider
       value={{
         fetchDnDSpells,
+        data,
       }}>
       {children}
     </DnDContext.Provider>
@@ -42,5 +57,6 @@ export const useDnDContext = () => {
 
   return {
     fetchDnDSpells: context.fetchDnDSpells,
+    spellsData: context.data.spellsData,
   };
 };
